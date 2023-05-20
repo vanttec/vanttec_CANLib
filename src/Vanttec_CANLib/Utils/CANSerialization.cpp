f9766124aff200558aa115203d30545c7395e893
EdisonAltamirano <A00825234@tec.mx>
2023-05-19 21:44:13 -0600

    can changes

--- src/Vanttec_CANLib/Utils/CANSerialization.cpp
+++ src/Vanttec_CANLib/Utils/CANSerialization.cpp
@@ -8,15 +8,15 @@
 
 uint32_t serialize_float(float input) {
     uint32_t out = 0;
-    out |= (input < 0) << 31; //sgn
+    unsigned int* ptr = (unsigned int*)&input; // Type cast float pointer to unsigned int pointer
+    unsigned int value = *ptr; // Get the binary representation of the float
 
-    int exp;
-    float frac = frexp(input, &exp);
-    uint8_t ieeeExp = (uint8_t) exp + 127;
-    uint32_t mant = fabs(frac) * 0x7fffff;
-
-    out |= ieeeExp << 23;
-    out |= mant & 0x7fffff;
+    int sign = (value >> 31) & 0x1; // Extract the sign bit (MSB)
+    int exponent = (value >> 23) & 0xFF; // Extract the exponent bits (bits 30-23)
+    int mantissa = value & 0x7FFFFF; // Extract the mantissa bits (bits 22-0)
+    out |= sign <<31;
+    out |= exponent << 23;
+    out |= mantissa & 0x7fffff;
     return out;
 }
 
@@ -27,7 +27,7 @@ void serialize_short(uint8_t *data, uint16_t in) {
 }
 
 void serialize_long(uint8_t *data, uint32_t in) {
-    in = vanttec_htonl(in);
+    //in = vanttec_htonl(in);
     data[0] = in >> (8 * 3);
     data[1] = in >> (8 * 2);
     data[2] = in >> 8;
