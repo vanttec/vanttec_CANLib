fd5224cce7acebf7818303677a209d488e7081cd
EdisonAltamirano <A00825234@tec.mx>
2023-05-30 14:41:08 -0600

    general can library for all devices

--- platform_specific/vanttec_canlib_stm/generic_tasks/generic_tasks.c
+++ platform_specific/vanttec_canlib_stm/generic_tasks/generic_tasks.c
@@ -1,8 +1,6 @@
 #include "generic_tasks/generic_tasks.h"
-#include "generic_tasks/requirements.h"
 #include "generic_tasks/can_reset_task.h"
 
 void canlib_init_generic_tasks(){
-	init_requirements_task();
 	init_can_reset_task();
 }
